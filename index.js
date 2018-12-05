3549f0798721c71545407c8625d74fbd9ad0ef85
silvermiguel96@gmail.com <silvermiguel96@gmail.com>
2018-12-05 12:56:31 -0500

    Implementación de modelos con sequelize

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = async function (config) {
+  const Agent = {}
+  const Metric = {}
+  //
+  return {
+    Agent,
+    Metric
+    // Use previous Emc Javascript
+    // Agent : Agent,
+    // Metric : Metric
+  }
+}
